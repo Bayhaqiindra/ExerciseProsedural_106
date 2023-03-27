2659fa84f9da7f65dac1c0924dd17385d2108100
bayhaqiindra <bayhaqi30072004@gmail.com>
2023-03-27 10:44:25 +0700

    Membuat dan mnampilkan status kelulusan

--- ExerciseProsedural_106/ExerciseProsedural_106.cpp
+++ ExerciseProsedural_106/ExerciseProsedural_106.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
 using namespace std;
 
+int a[20];
+int n;
+
 double rerata(double a, double b) {
 
 	return (a + b) / 2;
+}
+
+string status(double rata, double nil) {
+	if (rata >= 70 || nil >= 80)
+		return "Asroni Lulus";
+	else
+		return "Asroni Gagal";
+}
+string status2(double rata, double nil) {
+	if (rata >= 70 || nil >= 80)
+		return "Lia Lulus";
+	else
+		return "Lia Gagal";
+}
+
+string status3(double rata, double nil) {
+	if (rata >= 70 || nil >= 80)
+		return "Joko Lulus";
+	else
+		return "Joko Gagal";
+}
+
+int main() {
+	double nilM, nilB;
+	cout << "Masukkan nilai Matematika = ";
+	cin >> nilM;
+	cout << "Masukkan nilai Bahasa Inggris = ";
+	cin >> nilB;
+	cout << "Status kelulusan = " << status(rerata(nilM, nilB));
+	return 0;
 }
